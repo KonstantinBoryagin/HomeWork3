7900f90305c9c1718e04d7b3db5da7ddc4726111
Konstantin Boryagin <nokss89@gmail.com>
2021-07-21 23:35:57 +0300

    наброски

---
+++ src/ru/otus/Questions.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+public class Questions {
+    public static String[] questionOne = { "Сколько примитвных типов данных в Java?",
+                                           "1.  2",
+                                            "2.  5",
+                                            "3.  6" };
+
+    public static String[] questionTwo = { "С@@@",
+            "1.  2",
+            "2.  5",
+            "3.  6" };
+}
