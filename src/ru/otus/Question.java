b24b8217b12a25d243f314dcab2d57a59b765890
Konstantin Boryagin <nokss89@gmail.com>
2021-07-27 15:01:33 +0300

    prepare to delete old classes

--- src/ru/otus/Question.java
+++ src/ru/otus/Question.java
@@ -1,12 +1,14 @@
 package ru.otus;
 
+import java.util.Arrays;
+
 public enum Question {
     QUESTION_ONE (3, "Сколько примитивных типов данных в Java?",
-            new String[]{"1.  Десять", "2.  Пять", "3.  Восемь"}),
+            new String[] {"Десять", "Пять", "Восемь"}),
     QUESTION_TWO (4, "Какой из советских фильмов получил Оскар?",
-            new String[] {"1.  Кин-Дза-Дза","2.  Сталкер", "3.  Солярис", "4.  Москва слезам не верит"}),
+            new String[] {"Кин-Дза-Дза","Сталкер", "Солярис", "Москва слезам не верит"}),
     QUESTION_THREE (1, "Чем является Milky Way",
-            new String[] {"1.  Галактика", "2.  Скопление звёзд", "3.  Парад планет", "4.  Солнечная система", "5.  Шоколодка"});
+            new String[] {"Галактика", "Скопление звёзд", "Парад планет", "Солнечная система", "Шоколодка"});
 
 
     private int correct;
@@ -17,5 +19,44 @@ public enum Question {
         this.correct = correct;
         this.question = question;
         this.answerOptions = answerOptions;
+    }
+
+    public void printQuestion() {
+        System.out.println(question);
+        int formater = 0;
+        for (int i = 0; i < answerOptions.length; i++) {
+            System.out.printf("%3d"+ ". " + "%-20s", i + 1, answerOptions[i]);
+            formater++;
+            if (formater % 2 == 0) {
+                System.out.println();
+            }
+        }
+        //System.out.println();
+    }
+
+//    private int maxLength(String[] answerOptions) {
+//        int maxLength = 0;
+//        for (String string : answerOptions) {
+//            if (string.length() > maxLength) {
+//                maxLength = string.length();
+//            }
+//        }
+//        return maxLength;
+//    }
+
+    @Override
+    public String toString() {
+        return question + "\n" + Arrays.toString(answerOptions);
+    }
+    public int getCorrect() {
+        return correct;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getAnswerOptions() {
+        return answerOptions;
     }
 }
--- src/ru/otus/QuestionGenerator.java
+++ src/ru/otus/QuestionGenerator.java
@@ -1,5 +1,6 @@
 package ru.otus;
 
+
 public class QuestionGenerator {
     int trueAnswer;
     String question;
@@ -14,6 +15,17 @@ public class QuestionGenerator {
     }
 
 
+
+
+    public static void main(String[] args) {
+        Question questionOne = Question.QUESTION_ONE;
+        Question questionTwo = Question.QUESTION_TWO;
+        Question questionThree = Question.QUESTION_THREE;
+        questionOne.printQuestion();
+        questionTwo.printQuestion();
+        questionThree.printQuestion();
+        questionTwo.
+    }
     static QuestionGenerator stepOne = new QuestionGenerator(QuestionsDictionary.trueFirst, QuestionsDictionary.questionOne, QuestionsDictionary.answerOptionsFirst);
     static QuestionGenerator stepTwo = new QuestionGenerator(QuestionsDictionary.trueSecond, QuestionsDictionary.questionTwo, QuestionsDictionary.answerOptionsSecond);
     static QuestionGenerator stepThree = new QuestionGenerator(QuestionsDictionary.trueThird, QuestionsDictionary.questionThree, QuestionsDictionary.answerOptionsThird);
